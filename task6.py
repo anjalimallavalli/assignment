791a24cc077df27cc644266c139a74f5095d521d
anjalimallavalli <anjalimallavalli48@gmail.com>
2024-10-21 07:00:53 -0700

    task6

---
+++ task6.py
@@ -0,0 +1,169 @@
+#count()
+st="Python programming"
+print(st.count('o'))
+
+#Anagram
+st1="Elbow"
+st2="Below"
+st1=sorted(st1.lower())
+st2=sorted(st2.lower())
+print("st1 after sorting:,st1")
+print("st2 after sorting:,st2")
+if (st1==st2):
+    print("The strings are Anagrams")
+    else:
+        print("The strings are not anagrams")
+
+        #plindrome number or not
+        N="123321"
+        i=0
+        s=""
+        while i<len(N)
+        s=N[i]+s
+        i+=1
+        if N==s:
+            print("it is a palindrom")
+            else:
+                print("it is not a palindrom")
+
+                #replace the string
+                st="python programming"
+                new_st=st.replace("python","java programming")
+                print(new_st)
+
+                #replace the spaces in string with character
+                st="once upon a time"
+                char='-'
+                st=st.replace('',char)
+                print("string after replacing spaces with character:")
+                print(st)
+
+                #Lower case char to uppercase
+                s="python programming"
+                print(s.upper())
+
+                """Converting Vowels to uppercase
+                Name=input("Enter a string:")
+                res='aeiou'
+                for i in Name:
+                    if i in res:
+                        upr=i.upper()
+                        st=Name.replace(i,upr)
+                        print("After converting:",st)"""
+
+                        #separating character
+                        string="python"
+                        print("individual characters from given string")
+                        for i in range(0,len(string)):
+                            print(string[i],end="")
+
+                            #Removing Blank Spaces
+                            string="python programming"
+                            print(string.replace('',''))
+
+                            #string concatenation by using join()
+                            S1="Anjali"
+                            S2="Mallavalli"
+                            concat_result=""
+                            Concat_Result=Concat_Result.Join([s1+s2])
+                            print(concat_result)
+                            #string concatenation
+                            s1="python"
+                            s2="programming"
+                            s3=s1+s2
+                            print(s3)
+
+                            # finding  vowels or consonents
+                            ch='o'
+                            if ch in['a','e','i','o','u']:
+                                print("it is vowel")
+                                else:
+                                    print("it is a consonant")
+
+                                    #finding digit or not
+                                    ch="15"
+                                    if(ch.isdigit())
+                                    print("it is adigit")
+                                    else:
+                                        print("it is not a digit")
+
+                                        #count of vowels & consonants
+                                        input="hsaubqxnljdieurnpqrbnorn"
+                                        vlist=['a','e'.'i','o','u']
+                                        vCount=0
+                                        cCOUNT=0
+                                        for i in input:
+                                            if i in vlist:
+                                                vcount=vcount+1
+                                                else:
+                                                    ccount=ccount+1
+                                                    print("final count of vowels is:"+str(vCount))
+                                                    print("final count of consonent:"+str(ccount))
+
+                                                    #Highest frequency character
+                                                    str="This is really a string"
+                                                    print("Max character:" + max(str))
+
+                                                    #replacing character with'-'
+                                                    st="Green"
+                                                    new_st=st.replace("n","k")
+                                                    print(new_st)
+
+                                                    #counting Alphabets,digits,special characters
+                                                    st="Anjalimallavalli48@*&"
+                                                    Alphabets=digits.special=0
+                                                    for i in range(len(st)):
+                                                        if(st[i].isalpha()):
+                                                            alphabets=alphabets+1
+                                                            elif(st[i].isdigit()):
+                                                                digits=digits+1
+                                                                else:
+                                                                    special=special+1
+                                                                    print("total no of alphabets in this string",alphabets)
+                                                                    print("total no of digits in this string",digits)
+                                                                    print("total no of special in this string",special)
+
+                                                                    #finding digits without using of isdigit()
+                                                                    a="24546"
+                                                                    print(a.isdigit())
+
+                                                                    #sum of integers in a string
+                                                                    a="Anjali5454"
+                                                                    sum=0
+                                                                    for i in a:
+                                                                        if(i.isnumeric())
+                                                                        sum=int(i)
+                                                                        print(sum)
+
+                                                                        #non repeating character in string
+                                                                        s=("independance")
+                                                                        t=[]
+                                                                        for i in s:
+                                                                            if s.count(i)<2:
+                                                                                t.append(i)
+                                                                                print(t)
+                                                                                
+                                                                                #copying string
+                                                                                st1="Hello world"
+                                                                                st2=""
+                                                                                for i in st1:
+                                                                                    st2=st2+i:
+                                                                                    print("The final string st2=",st2)
+                                                                                
+
+
+
+                                                    
+
+
+
+
+
+                            
+
+
+
+
+
+                
+
