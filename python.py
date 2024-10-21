28cade938a7c51d764782a22f99a6efa32bc9e0e
anjalimallavalli <anjalimallavalli48@gmail.com>
2024-10-21 07:04:12 -0700

    python

---
+++ python.py
@@ -0,0 +1,13 @@
+a=int(input("Enter Balance:"))
+b=int(input("enter Deposite:"))
+c=int(input("Enter Withdraw:"))
+if(a<0):
+    print("Balance")
+    elif(b<=a):
+        print("Deposite")
+        elif(c>b):
+            print("Withdraw")
+            else:
+                print("Exit")
+
+
